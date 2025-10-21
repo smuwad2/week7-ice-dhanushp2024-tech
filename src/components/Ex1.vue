18241990b86a450376b2548fa691985a05c51027
dhanushp2024-tech <dhanush.p.2024@smu.edu.sg>
2025-10-21 01:05:14 +0000

    ex1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,24 +1,48 @@
 <script>
-    export default { 
-        // Add Code Here to complete the task
-        // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+export default {
+    data() {
+        return {
+            x: 0,
+            y: 0,
+            selectedOp: '+',
+            operators: ['+', '-', '*', '/', '%']
+        };
+    },
+    computed: {
+        result() {
+            switch (this.selectedOp) {
+                case '+':
+                    return this.x + this.y;
+                case '-':
+                    return this.x - this.y;
+                case '*':
+                    return this.x * this.y;
+                case '/':
+                    // Handle division by zero
+                    return this.y === 0 ? 'Error' : this.x / this.y;
+                case '%':
+                    // Handle modulo by zero
+                    return this.y === 0 ? 'Error' : this.x % this.y;
+                default:
+                    return 'Unknown operator';
+            }
+        }
     }
+}
 </script>
 
 <template>
     <p>x <input v-model.number="x"></p>
     <select v-model="selectedOp">
-        <option v-for="op in operators" >{{ op }}</option>
+        <option v-for="op in operators" :key="op">{{ op }}</option>
     </select>
     <p>y <input v-model.number="y"></p>
-    
-    <p>---------------------</p>
 
-    <p>= {{result}}</p>
+    <p>---------------------</p>
+    <p>= {{ result }}</p>
 </template>
 
 <style scoped>
     p, input { font-family: monospace; }
     p { white-space: pre; }
-
 </style>
